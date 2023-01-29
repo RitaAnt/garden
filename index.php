9be80c097e24a7728e42c896d991523dd3622f81
root <kolotvinova03@bk.ru>
2023-01-29 21:33:27 +0300

    bones

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
 
+class Tree
+{
+    public function __clone()
+    {
 
+     }
+}
+class AppleTree
+{
+     public function getClone(Tree $tree)
+    {
+         return clone $tree;
+    }
+}
+class PearTree
+{
+    public function getClone(Tree $tree)
+    {
+
+    }
+}
 
 
 ?>
